b26d41946c0863c9bfac218e952524c1f67aa72f
Stepheno <fireflyengineer@gmail.com>
2025-12-12 16:29:48 +0800

    feat: update schema

--- apps/nextjs/src/auth/server.ts
+++ apps/nextjs/src/auth/server.ts
@@ -19,8 +19,9 @@ export const auth = initAuth({
   baseUrl,
   productionUrl: `https://${env.VERCEL_PROJECT_PRODUCTION_URL ?? "turbo.t3.gg"}`,
   secret: env.AUTH_SECRET,
-  discordClientId: env.AUTH_DISCORD_ID,
-  discordClientSecret: env.AUTH_DISCORD_SECRET,
+  googleClientId: env.GOOGLE_CLIENT_ID,
+  googleClientSecret: env.GOOGLE_CLIENT_SECRET,
+
   extraPlugins: [nextCookies()],
 });
 
